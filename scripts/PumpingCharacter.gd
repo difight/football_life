65cc32897fd4bd7c1c9c0850465ee3f601b5ff9c
difight <greed_nord@mail.ru>
2021-09-03 19:35:38 +0200

    Pumping

--- scripts/PumpingCharacter.gd
+++ scripts/PumpingCharacter.gd
@@ -3,18 +3,28 @@ extends Control
 onready var ListPumping = get_node("ScrollContainer/ListPumping")
 var PumpingCharacterItem = preload("res://scenes/PumpingCharacterItem.tscn")
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
+dy():
 	var newItem = PumpingCharacterItem.instance()
-	newItem.find_node("HBoxContainer/Text")
 	ListPumping.add_child(newItem)
+	var Text = newItem.get_node("HBoxContainede("HBoxContainer/Button")
+	var 	var Text = newItem.get_node("HBoxContainer/Text")
+	var ButtonPush = newItem.get_node("HBoxContainer/Button")
+	var name = "tttt"
+	Text.text = '123'
+	ButtonPush.text = 'push'
+	ButtonPush.connect("pressed", self, "_on_pumping_pressed", [name, ButtonPush])
+name = "tttt"
+	Text.text = '123'
+	Butfunc _on_pumping_pressed(_name:String, _buttonpush:Button):
+	print("name = ", name)
+	_buttonpush.disabled = true
+tonPush.text = 'push'
+	ButtonPush.connect("pressed", self, "_on_pumping_pressed", [name, ButtonPush])
 	pass # Replace with function body.
 
+func _on_pumping_pressed(_name:String, _buttonpush:Button):
+	print("name = ", name)
+	_buttonpush.disabled = true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
